5d0e5489c6020c32055e89a446fd424da44a407b
kyounopopi <keisuke.isioka.0514@icloud.com>
2025-11-21 14:21:03 +0900

    階層移動

--- tiisanaproject/kirokuapp/apps.py
+++ tiisanaproject/kirokuapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TiisanaappConfig(AppConfig):
+class KirokuappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "tiisanaapp"
+    name = "kirokuapp"
