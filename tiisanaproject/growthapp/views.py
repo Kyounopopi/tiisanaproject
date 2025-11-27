53c451e021eb6e121a0ae6408b69e6a8ed3a4967
kyounopopi <keisuke.isioka.0514@icloud.com>
2025-11-27 15:31:05 +0900

    写真アプリの初期設定とアイコンの追加、ログインページのアイコン変更、CSSスタイルの追加

--- tiisanaproject/growthapp/views.py
+++ tiisanaproject/growthapp/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render, redirect
-from .models import GrowthRecord
+# from .models import GrowthRecord
 
-def growth_view(request):
+# def growth_view(request):
 
-    # POST（データ登録）
-    if request.method == "POST":
-        date = request.POST.get("date")
-        height = request.POST.get("height")
-        weight = request.POST.get("weight")
-        development = request.POST.get("development")
-        comment = request.POST.get("comment")
+#     # POST（データ登録）
+#     if request.method == "POST":
+#         date = request.POST.get("date")
+#         height = request.POST.get("height")
+#         weight = request.POST.get("weight")
+#         development = request.POST.get("development")
+#         comment = request.POST.get("comment")
 
-        # DBに保存
-        GrowthRecord.objects.create(
-            date=date,
-            height=height,
-            weight=weight,
-            development=development,
-            comment=comment
-        )
+#         # DBに保存
+#         GrowthRecord.objects.create(
+#             date=date,
+#             height=height,
+#             weight=weight,
+#             development=development,
+#             comment=comment
+#         )
 
-        # 登録後はリロード（F5押しても二重登録を防ぐ）
-        return redirect("growth")
+#         # 登録後はリロード（F5押しても二重登録を防ぐ）
+#         return redirect("growth")
 
-    # GET（画面表示）
-    growthapp = GrowthRecord.objects.all().order_by("-date")
+#     # GET（画面表示）
+#     growthapp = GrowthRecord.objects.all().order_by("-date")
 
-    return render(request, "growthapp.html", {
-        "growthapp": growthapp
-    })
+#     return render(request, "growthapp.html", {
+#         "growthapp": growthapp
+#     })
