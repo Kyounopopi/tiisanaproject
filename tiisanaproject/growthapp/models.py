e80278a0b85c2bb7e37bc63760f366bcecbbb1a5
木村翼佐 <mit2471575@stu.o-hara.ac.jp>
2025-11-28 10:09:33 +0900

    成長記録画面の作成

--- tiisanaproject/growthapp/models.py
+++ tiisanaproject/growthapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class GrowthRecord(models.Model):
+    date = models.DateField(verbose_name="日付")
+    height = models.FloatField(verbose_name="身長（cm）")
+    weight = models.FloatField(verbose_name="体重（kg）")
+    baby_log = models.TextField(max_length=150, verbose_name="発達状況")
+    comment = models.TextField(blank=True, null=True, verbose_name="コメント")
+
+    def __str__(self):
+        return f"{self.date} の成長記録"
+
 # Create your models here.
