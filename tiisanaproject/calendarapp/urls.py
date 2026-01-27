935d44b21770fb7deeba40bb5a2d0f451c2dde17
永井志臣 <scwxs1108@gmail.com>
2026-01-27 13:52:12 +0900

    コメント追加機能の修正

--- tiisanaproject/calendarapp/urls.py
+++ tiisanaproject/calendarapp/urls.py
@@ -5,6 +5,6 @@ app_name = 'calendarapp'
 
 urlpatterns = [
     path('currender/', views.calendar_view, name='currender'),
-    path('add/', views.add_record, name='add_record'),
+    path('add_record/', views.add_record, name='add_record'),
     path("comment/<int:record_id>/", views.edit_comment, name="edit_comment"),
 ]
