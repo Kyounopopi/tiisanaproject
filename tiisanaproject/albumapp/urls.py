573baa798a27bea347eb73ff74b599ded565bb57
kyounopopi <keisuke.isioka.0514@icloud.com>
2026-01-28 11:09:28 +0900

    アルバム詳細ページの作成

--- tiisanaproject/albumapp/urls.py
+++ tiisanaproject/albumapp/urls.py
@@ -11,6 +11,7 @@ app_name = "albumapp"
 urlpatterns = [
     path("album", AlbumListView.as_view(), name="album_list"),
     path("create/", AlbumCreateView.as_view(), name="album_create"),
-    path("<int:pk>/", AlbumDetailView.as_view(), name="album_detail"),
+    path("<int:pk>/", AlbumDetailView.as_view()),
+    path("<slug:slug>/", AlbumDetailView.as_view() ,name="album_detail"),
     path("favorite/<int:photo_id>/", ToggleFavoriteView.as_view(), name="toggle_favorite"),
 ]
