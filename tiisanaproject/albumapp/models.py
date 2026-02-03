d3266d8c4e5c6a00a2490cfe5500dfe4fec398ca
kyounopopi <keisuke.isioka.0514@icloud.com>
2026-02-03 10:12:49 +0900

    index画面の変更

--- tiisanaproject/albumapp/models.py
+++ tiisanaproject/albumapp/models.py
@@ -12,13 +12,6 @@ class Album(models.Model):
     slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
 
 class Photo(models.Model):
