a8ab3a25b02ec03ee16e0a4320aed2eb65904e31
kyounopopi <keisuke.isioka.0514@icloud.com>
2026-02-04 12:24:51 +0900

    エラー修正

--- tiisanaproject/albumapp/urls.py
+++ tiisanaproject/albumapp/urls.py
@@ -11,7 +11,6 @@ app_name = "albumapp"
 urlpatterns = [
     path("album/", AlbumListView.as_view(), name="album_list"),
     path("create/", AlbumCreateView.as_view(), name="album_create"),
-    path("<int:pk>/", AlbumDetailView.as_view()),
-    path("<slug:slug>/", AlbumDetailView.as_view() ,name="album_detail"),
+    path("<int:pk>/", AlbumDetailView.as_view() ,name="album_detail"),
     path("favorite/<int:photo_id>/", ToggleFavoriteView.as_view(), name="toggle_favorite"),
 ]
