2e2b1460e8f254254e63f2bb71a2e507150255c3
kyounopopi <keisuke.isioka.0514@icloud.com>
2026-02-12 10:57:35 +0900

    エラー修正

--- tiisanaproject/albumapp/models.py
+++ tiisanaproject/albumapp/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.conf import settings
 
 class Album(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="albums"
+    )
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
